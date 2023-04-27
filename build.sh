69836ea4b20a30113be33b3136301c06993e2501
Christian Bull <cbull.projects@gmail.com>
2023-04-27 14:56:05 -0400

    add build instructions

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ podman push csbul55/catfacts:v7
 
 platarch=linux/amd64,linux/arm64
 buildah build --jobs=2 --platform=$platarch --manifest catfacts .
+buildah tag localhost/catfacts csbull55/catfacts:v10
+podman manifest rm localhost/catfacts
+podman manifest push --all csbull55/catfacts:v10 docker://csbull55/catfacts:v10
