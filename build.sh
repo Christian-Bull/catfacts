10361f4aced76d5d202702af32eef420f410ed06
Christian Bull <cbull.projects@gmail.com>
2023-04-27 12:25:46 -0400

    update build process, push new version:

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 podman buildx build -t csbul55/catfacts:v7 .
 podman buildx build -t catfacts:v2 .
 podman push csbul55/catfacts:v7
+
+
+platarch=linux/amd64,linux/arm64
+buildah build --jobs=2 --platform=$platarch --manifest catfacts .
